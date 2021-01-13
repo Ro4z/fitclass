7eb256bbc985eb0173377bf06225e0bcfe9bfc71
You In Seong <insung1217@gmail.com>
2021-01-13 18:40:44 +0900

    feat: Add component using in PTClass content

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -3,12 +3,14 @@ import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 import WorkoutStudyContainer from '@components/main/WorkoutStudyContainer';
 import BannerImage from '@components/main/BannerImage';
+import PTClassContainer from '@components/main/PTClassContainer';
 
 function MainScreen() {
   return (
     <SafeAreaView style={styles.mainContainer}>
       <WorkoutStudyContainer />
       <BannerImage />
+      <PTClassContainer />
     </SafeAreaView>
   );
 }
