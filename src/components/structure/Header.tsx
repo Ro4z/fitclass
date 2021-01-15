9f24eb6b61b1b4701635c1a5f460d1674ca06ca9
Dongsik Ga <ret0422@gmail.com>
2021-01-15 18:04:10 +0900

    Add search icon & align header

--- src/components/structure/Header.tsx
+++ src/components/structure/Header.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import MenuIcon from 'react-native-vector-icons/Entypo';
+import SearchIcon from 'react-native-vector-icons/Feather';
+
 MenuIcon.loadFont();
+SearchIcon.loadFont();
 
 type HeaderProps = {
   title: string;
@@ -11,6 +14,7 @@ function Header({title = ''}: HeaderProps) {
     <View style={styles.header}>
       <MenuIcon name="menu" style={styles.menuIcon} size={30} />
       <Text style={styles.title}>{title}</Text>
+      <SearchIcon name="search" style={styles.searchIcon} size={20} />
     </View>
   );
 }
@@ -21,16 +25,22 @@ const styles = StyleSheet.create({
     height: 55,
     backgroundColor: 'rgb(50,50,50)',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     flexDirection: 'row',
   },
   title: {
     fontSize: 18,
     fontFamily: 'OpenSauceSans-Bold',
     color: '#fff',
+    textAlign: 'center',
   },
   menuIcon: {
     color: '#ffffff',
+    marginLeft: 15,
+  },
+  searchIcon: {
+    color: '#ffffff',
+    marginRight: 15,
   },
 });
 
