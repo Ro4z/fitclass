58fdb7197926d4acbaa06e7507950453f381dc91
You In Seong <insung1217@gmail.com>
2021-01-17 18:18:52 +0900

    feat: Add component for PopularStudy

---
+++ src/components/study/PopularStudyContainer.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
+import ContentTitle from '@components/main/ContentTitle';
+import PopularStudyList from './PopularStudyList';
+
+function PopularStudyContainer({navigation}: {navigation?: any}) {
+  return (
+    <View style={styles.mainContainer}>
+      <ContentTitle
+        title="TOP 3 ZOOM 독서실"
+        subTitle="TOP 참여율 FITCLASS ZOOM"
+      />
+      <PopularStudyList />
+    </View>
+  );
+}
+
+export default PopularStudyContainer;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    height: 330,
+    marginBottom: 30,
+  },
+});
