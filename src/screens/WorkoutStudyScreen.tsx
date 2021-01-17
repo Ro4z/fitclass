6493320490048fd218c877361c85f0277deabacb
You In Seong <insung1217@gmail.com>
2021-01-17 16:14:56 +0900

    feat: Add WorkoutStudyScreen

---
+++ src/screens/WorkoutStudyScreen.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+import {BACKGROUND} from '@styles/color';
+
+function WorkoutStudyScreen() {
+  return (
+    <View style={styles.mainContainer}>
+      <Text></Text>
+    </View>
+  );
+}
+
+export default WorkoutStudyScreen;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: BACKGROUND,
+  },
+});
